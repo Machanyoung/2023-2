37a33b01b9755fe72e22f6ff2e5649c7b7d4b74f
Machanyoung <tuna0317@smail.kongju.ac.kr>
2023-09-07 15:46:57 +0900

    3* print

--- prectice0/prectice0.cpp
+++ prectice0/prectice0.cpp
@@ -1,7 +1,20 @@
 ﻿#include <stdio.h>
-
-int main()
+int main(void)
 {
-	printf("hello world!\n");
+	int a;
+	int b;
+	int c;
+
+	for (a = 0; a < 3; a++)
+	{
+		for (b = a; b <= 5 - 1; b++) {
+			printf(" ");
+		}
+		for (c = 0; c <= (a * 2); c++) {
+			printf("*");
+		}
+		printf("\n");
+	}
+
 	return 0;
 }
