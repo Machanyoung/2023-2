11e5a889866caa82740238ba4dfdbcdbfc68c945
Machanyoung <tuna0317@smail.kongju.ac.kr>
2023-09-21 16:09:04 +0900

    Create lecture5.cpp
    몬테 카를로 알고리즘

---
+++ c프로그래밍 실습 3주차/c프로그래밍 실습 3주차/lecture5.cpp
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+
+
+int main()
+{
+	int i = 0;
+	double x = 0;
+	double y = 0;
+	double circle = 0;
+	double count = 0;
+
+	srand((double)time(NULL));
+	while (1)
+	{
+		for (i = 0; i < 100000; i++)
+		{
+			x = (double)rand() / (double)RAND_MAX;
+			y = (double)rand() / (double)RAND_MAX;
+			count++;
+			if ((x * x) + (y * y) <= 1)
+			{
+				circle++;
+			}
+		}
+	}
+
+	printf("%0.0f, ¿øÁÖÀ² = %0.15f\n", count, (circle / count) * 4);
+}
