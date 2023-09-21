bfdd6af3f55de98b76b649d0e162660565322deb
Machanyoung <tuna0317@smail.kongju.ac.kr>
2023-09-21 14:27:21 +0900

    Create lecture1.cpp
    소수판별

---
+++ c프로그래밍 실습 3주차/c프로그래밍 실습 3주차/lecture1.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int isPrime(int num)
+{
+	for (int div = num-1 > 1; div--)
+	{
+		if (num % dic == 0) return 0;
+	}
+	return 1;
+}
+int main(void)
+{
+	int num;
+	scanf_s("%d", &num)
+}
