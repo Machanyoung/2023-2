ba8b39ed643dda14d43127879987d39d6a832456
Machanyoung <tuna0317@smail.kongju.ac.kr>
2023-10-19 15:09:02 +0900

    메모리 동적할당 실습2(구조체)

---
+++ c프로그래밍 실습 7주차/C프로그래밍 실습 7주차/lecture2.cpp
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct Student {
+	int sno;
+	char name[10];
+	int score;
+};
+int main() {
+	int num_student;
+	printf("학생 수 입력 :");
+	scanf_s("%d", &num_student);
+
+	struct Student* std;
+	std = (int*)malloc(sizeof(int) * num_student);
+	for (int i = 0; i < num_student; i++) {
+		printf("학생 # %d-%d 학번 입력 :", num_student, i + 1);
+		scanf_s("%d", &(std[i].sno));
+		printf("학생 # %d-%d 이름 입력 :", num_student, i + 1);
+		scanf_s("%s", &(std[i].name,10));
+		printf("학생 # %d-%d 성적 입력 :", num_student, i + 1);
+		scanf_s("%d", &(std[i].score));
+	}
+	int sum = 0;
+	for (int i = 0; i < num_student; i++) {
+		sum += &(std[i].score);
+	}
+	printf("총점 : %d", sum);
+}
+//실습 진행중...
