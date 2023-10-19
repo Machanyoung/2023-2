e747bd59141197c04c2950ae08cbf1823397d820
Machanyoung <tuna0317@smail.kongju.ac.kr>
2023-10-19 15:18:26 +0900

    에러수정

--- c프로그래밍 실습 7주차/C프로그래밍 실습 7주차/lecture2.cpp
+++ c프로그래밍 실습 7주차/C프로그래밍 실습 7주차/lecture2.cpp
@@ -12,19 +12,18 @@ int main() {
 	scanf_s("%d", &num_student);
 
 	struct Student* std;
-	std = (int*)malloc(sizeof(int) * num_student);
+	std = (struct Student*)malloc(sizeof(struct Student) * num_student);
 	for (int i = 0; i < num_student; i++) {
 		printf("학생 # %d-%d 학번 입력 :", num_student, i + 1);
 		scanf_s("%d", &(std[i].sno));
 		printf("학생 # %d-%d 이름 입력 :", num_student, i + 1);
-		scanf_s("%s", &(std[i].name,10));
+		scanf_s("%s", (std[i].name),10);
 		printf("학생 # %d-%d 성적 입력 :", num_student, i + 1);
 		scanf_s("%d", &(std[i].score));
 	}
 	int sum = 0;
 	for (int i = 0; i < num_student; i++) {
-		sum += &(std[i].score);
+		sum += std[i].score;
 	}
 	printf("총점 : %d", sum);
 }
-//실습 진행중...
